232ed49fcb96ca69bd79c1805d7fe84f439e16e1
Fabio Guarino <fabio.guarino@hofspannung.de>
2024-07-18 14:33:27 +0200

    working new ui

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,10 @@
 #include "../lib/hal_stm_lvgl/touchpad/touchpad.h"
 #include "../lib/hal_stm_lvgl/tft/tft.h"
 #include "../lib/display/src/ui.h"
+// #include "canService.cpp"
 
-// Define CAN interface pins
-#define CAN_RD PB_8  // Adjust according to your hardware
-#define CAN_TD PB_9
-#define LVGL_TICK 10     // Time tick value for lvgl in ms (1-10msa)
-#define TICKER_TIME 0.01 // modified to miliseconds
+// Define can1 interface pins
+#define LVGL_TICK 30 // Time tick value for lvgl in ms (1-10msa)
 
 /*
  * Copyright (c) 2017-2020 Arm Limited and affiliates.
@@ -16,82 +14,85 @@
  */
 
 #if !DEVICE_CAN
-#error [NOT_SUPPORTED] CAN not supported for this target
+#error[NOT_SUPPORTED] CAN not supported for this target
 #endif
 
+#define DEBUG_CAN 1
 
 Ticker ticker;
-DigitalOut led1(LED1);
-/** The constructor takes in RX, and TX pin respectively.
-  * These pins, for this example, are defined in mbed_app.json
-  */
-CAN can1(CAN_RD, CAN_TD);
-char counter = 0;
-    static void lv_ticker_func()
-    {
-        lv_tick_inc(LVGL_TICK);
-    }
-
-
-void send()
-{
-    printf("send()\n");
-    if (can1.write(CANMessage(80, &counter, 1))) {
-        printf("wloop()\n");
-        counter++;
-        printf("Message sent: %d\n", counter);
-    }
-    led1 = !led1;
-}
-
 Thread thread;
+
+#define CAN_RD PB_8 // Adjust according to your hardware
+#define CAN_TD PB_9
+
+CAN can1(CAN_RD, CAN_TD, 100000);
 
 void send_thread()
 {
-    while (true) {
-        send();
-        ThisThread::sleep_for(0.1);
+
+    CANMessage msg(80, "1", 1);
+    while (true)
+    {
+#ifdef DEBUG
+        printf("send()\n");
+#endif
+
+        if (can1.write(msg))
+        {
+#ifdef DEBUG
+            printf("wloop()\n");
+            printf("Message sent: %d\n", msg.id);
+#endif
+        }
+        ThisThread::sleep_for(1s);
     }
 }
 
+void lv_ticker_thread()
+{
+    while (true)
+    {
+#ifdef DEBUG
+        printf("lv_ticker_thread()\n");
+#endif
+        lv_task_handler();
+
+        lv_tick_inc(LVGL_TICK);
+        ThisThread::sleep_for(LVGL_TICK);
+    }
+}
 
 int main()
 {
 
-    can1.frequency(100000);
-    can1.mode(CAN::Normal);
-
     printf("main()\n");
     CANMessage msg;
+
+    can1.filter(291, 0xFFF, CANStandard, 0);
 
     // Lvgl init
     lv_init();
     tft_init();
     touchpad_init();
 
-    ticker.attach(callback(&lv_ticker_func), TICKER_TIME); // Attach callback to ticker
+    // Create a thread to handle lvgl tasks
+    thread.start(lv_ticker_thread);
+    // thread.start(lv_task_handler_thread);
+    // thread.start(send_thread);
+
     ui_init();
 
 
-    thread.start(send_thread);
+    while (1)
+    {
+#ifdef DEBUG_CAN
+        if (can1.read(msg))
+        printf("Message received: %d\n", msg.id);
+#else
+        can1.read(msg);
 
-    while (1) {
-        if (can1.read(msg)) {
-            printf("Message received: %d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n", msg.id, msg.data[0], msg.data[1], msg.data[2], msg.data[3], msg.data[4], msg.data[5], msg.data[6], msg.data[7]);
-            send();
-        }
-        if(can1.rderror()) {
-            printf("CAN read error\n");
-            can1.reset();
-        }
-        if(can1.tderror()) {
-            printf("CAN write error\n");
-            can1.reset();
-        }
+#endif
 
-
-        ThisThread::sleep_for(0.1);
+        ThisThread::sleep_for(1ms);
     }
 }
-
-
